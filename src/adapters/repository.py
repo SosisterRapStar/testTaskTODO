986f6291b4abb2052c6cdda7cf7bdfa8b2e372da
SosisterRapStar <timoshenkovvanya@gmail.com>
2024-09-16 02:01:02 +0300

    added_abstractions

--- src/adapters/repository.py
+++ src/adapters/repository.py
@@ -1,9 +1,14 @@
 from dataclasses import dataclass
 from abc import ABC, abstractmethod
-
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Type, List, ClassVar
+from src.domain.orm import Base
 
 @dataclass
 class AbstractRepo(ABC):
+    session: AsyncSession
+    _model: ClassVar[Type[Base]] = None
+
     @abstractmethod
     async def create(self):
         raise NotImplementedError
@@ -15,3 +20,23 @@ class AbstractRepo(ABC):
     @abstractmethod
     async def update(self):
         raise NotImplementedError
+    
+    @abstractmethod
+    async def get(self):
+        raise NotImplementedError
+    
+    @abstractmethod
+    async def get_list(self):
+        raise NotImplementedError
+
+@dataclass
+class AbstractNotesRepo(AbstractRepo):
+    
+    @abstractmethod
+    async def get_by_tags(self):
+        raise NotImplementedError
+    
+
+@dataclass
+class AbstractUserRepo(AbstractRepo):
+    pass
