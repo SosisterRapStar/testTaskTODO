e58aa8b6b1d6783455c67dc4ecdaaf56998f263e
SosisterRapStar <timoshenkovvanya@gmail.com>
2024-09-16 00:07:29 +0300

    added_some_token_logic

--- src/authorization /security.py
+++ src/authorization /security.py
@@ -1,15 +1,9 @@
 from passlib.context import CryptContext
-from dataclasses import dataclass
+from .exceptions import PasswordVerificationError
 
 
 
-@dataclass
-class PasswordVerificationError(Exception):
-    message: str
-    def __str__(self) -> str:
-        return super().__str__()
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+crypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 
@@ -17,6 +11,11 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 # хэшированный пароль должен поступать из базы данных
-async def check_passwords(raw_password: str, hashed_password: str) -> bool:
-    if not pwd_context.verify(raw_password, hashed_password):
+async def check_passwords(raw_password: str, hashed_password: str) -> None:
+    if not crypt_context.verify(raw_password, hashed_password):
         raise PasswordVerificationError
+
+
+async def hash_password(raw_password: str) -> str:
+    return crypt_context.hash(raw_password)
+
--- src/authorization /tokens.py
+++ src/authorization /tokens.py
@@ -2,10 +2,14 @@ from datetime import timedelta, datetime
 from typing import Any
 
 from jose import jwt
-from passlib.context import CryptContext
+
 
 from src.config import settings
 
 
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+async def create_token(user_data: dict[str, Any], expiration_time: timedelta):
+    payload = user_data.copy()
+    expire_in = datetime.now() + expire
+    payload.update({"iat": datetime.now(), "exp": expire_in})
+    return payload
