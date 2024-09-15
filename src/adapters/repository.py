f1c0ef6c8f871c3f68a2d17c93182ccc683b2317
SosisterRapStar <timoshenkovvanya@gmail.com>
2024-09-15 20:33:47 +0300

    refactored_packages

---
+++ src/adapters/repository.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from abc import ABC
+
+
+@dataclass
+class AbstractRepo(ABC):
+
+    async def create(self):
+        raise NotImplementedError
+    
+    async def delete(self):
+        raise NotImplementedError
+    
+    async def update(self):
+        raise NotImplementedError
+    
+
+
--- src/app.py
+++ src/app.py
@@ -13,14 +13,19 @@ app = FastAPI()
 async def get_notes():
     pass
 
-
+@app.post()
 async def create_note():
     pass
 
-
+@app.put()
 async def update_note():
     pass
 
-
+@app.delete()
 async def delete_note():
     pass
+
+@app.post()
+async def add_tag_to_note():
+    pass
+
